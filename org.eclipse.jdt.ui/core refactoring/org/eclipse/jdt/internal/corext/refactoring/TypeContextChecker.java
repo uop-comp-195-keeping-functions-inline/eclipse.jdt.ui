a0b8304311fc1c0a1eb9da64239a37a4086add9e
Noopur Gupta <noopur_gupta@in.ibm.com>
2013-09-05 17:38:04 +0530

    Fixed bug 409719: [1.8][refactoring] Incorrect Method signature preview for default methods

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/TypeContextChecker.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/TypeContextChecker.java
@@ -1,9 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This is an implementation of an early-draft specification developed under the Java Community Process (JCP) and
+ * is made available for testing and evaluation purposes only.
+ * The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -217,8 +221,12 @@ public class TypeContextChecker {
 		}
 
 		private int appendMethodDeclaration(StringBuffer cuString, String[] types, int parameterCount) throws JavaModelException {
-			if (Flags.isStatic(fMethod.getFlags()))
+			int flags= fMethod.getFlags();
+			if (Flags.isStatic(flags)) {
 				cuString.append("static "); //$NON-NLS-1$
+			} else if (Flags.isDefaultMethod(flags)) {
+				cuString.append("default "); //$NON-NLS-1$
+			}
 
 			ITypeParameter[] methodTypeParameters= fMethod.getTypeParameters();
 			if (methodTypeParameters.length != 0) {
