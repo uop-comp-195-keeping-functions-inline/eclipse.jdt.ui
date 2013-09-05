d7e0983c1bde492aef5ef56a395072d3fafa75fc
Noopur Gupta <noopur_gupta@in.ibm.com>
2013-09-05 18:45:24 +0530

    Commented ExceptionOccurrencesFinder.methodThrowsException(MethodDeclaration method, Name exception) temporarily

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/ExceptionOccurrencesFinder.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/ExceptionOccurrencesFinder.java
@@ -1,10 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -15,7 +19,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTMatcher;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
@@ -117,12 +120,12 @@ public class ExceptionOccurrencesFinder extends ASTVisitor implements IOccurrenc
 	}
 
 	private boolean methodThrowsException(MethodDeclaration method, Name exception) {
-		ASTMatcher matcher = new ASTMatcher();
+		/*ASTMatcher matcher = new ASTMatcher();
 		for (Iterator<Name> iter = method.thrownExceptions().iterator(); iter.hasNext();) {
 			Name thrown = iter.next();
 			if (exception.subtreeMatch(matcher, thrown))
 				return true;
-		}
+		}*/
 		return false;
 	}
 
