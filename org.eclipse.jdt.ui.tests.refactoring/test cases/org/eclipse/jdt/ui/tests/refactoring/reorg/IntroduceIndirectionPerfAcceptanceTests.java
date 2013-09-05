36d99828a30ceeda09de6d664e47bb807714c2cd
Noopur Gupta <noopur_gupta@in.ibm.com>
2013-09-05 18:03:19 +0530

    Fixed bug 409723: [1.8][introduce indirection] Unable to introduce indirection on methods in an interface

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/reorg/IntroduceIndirectionPerfAcceptanceTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/reorg/IntroduceIndirectionPerfAcceptanceTests.java
@@ -1,16 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.ui.tests.refactoring.reorg;
 
-import junit.framework.Assert;
 import junit.framework.Test;
 
 import org.eclipse.test.OrderedTestSuite;
@@ -55,10 +58,10 @@ public class IntroduceIndirectionPerfAcceptanceTests extends RefactoringPerforma
 
 		IType control= fProject.findType("org.eclipse.swt.widgets.Widget");
 		IMethod m= control.getMethod("getDisplay", new String[0]);
-		Assert.assertTrue(m != null && m.exists());
+		assertTrue(m != null && m.exists());
 		fRefactoring= new IntroduceIndirectionRefactoring(m);
 		fRefactoring.setEnableUpdateReferences(true);
-		fRefactoring.setIntermediaryClassName("org.eclipse.swt.widgets.Widget");
+		fRefactoring.setIntermediaryTypeName("org.eclipse.swt.widgets.Widget");
 		fRefactoring.setIntermediaryMethodName("bar");
 	}
 
