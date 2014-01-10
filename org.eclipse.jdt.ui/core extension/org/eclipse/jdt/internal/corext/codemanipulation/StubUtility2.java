54860f6e13f11607c34936cf39ab4495e5223bfd
Noopur Gupta <noopur_gupta@in.ibm.com>
2014-01-10 14:24:01 +0530

    Fixed bug 425006: [1.8][quick assist] 'Convert to anonymous class creation' creates wildcard return type

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility2.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -331,6 +331,10 @@ public final class StubUtility2 {
 		decl.setConstructor(false);
 		
 		ITypeBinding bindingReturnType= binding.getReturnType();
+		if (bindingReturnType.isWildcardType()) {
+			ITypeBinding bound= bindingReturnType.getBound();
+			bindingReturnType= (bound != null) ? bound : bindingReturnType.getErasure();
+		}
 		
 		if (JavaModelUtil.is50OrHigher(unit.getJavaProject())) {
 			createTypeParameters(imports, context, ast, binding, decl);
